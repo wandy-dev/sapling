99083ba6933c45f698636b92986f8ac29d022afe
Andy DeVane <w.andrew.dev@gmail.com>
2025-11-03 19:46:01 -0500

    reverse post order in main timeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
   def index
-    @posts = Post.original_post.order(created_at: :asc)
+    @posts = Post.original_post.order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json
