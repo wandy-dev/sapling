3b2801200da1ab2df4eb45c155e1038512bbcefb
Andy DeVane <w.andrew.dev@gmail.com>
2025-11-11 13:45:28 -0500

    fix post create turbo stream

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,7 @@ class PostsController < ApplicationController
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
+        format.turbo_stream
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
