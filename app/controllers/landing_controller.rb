fc724dc6922bbfb6dc94e656d7b776561b26c85b
Andy DeVane <w.andrew.dev@gmail.com>
2025-11-21 14:50:59 -0500

    add landing page redirect

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,13 @@
 class LandingController < ApplicationController
+  before_action :go_home
   def index
+  end
+
+  private
+
+  def go_home
+    if user_signed_in?
+      redirect_to posts_path, status: 302
+    end
   end
 end
